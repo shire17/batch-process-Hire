1fccb1862b435056491d03f0e55289d346977a9b
Shannon Hire <shire@ip-172-31-26-81.us-east-2.compute.internal>
2020-10-01 00:28:36 +0000

    Initial commit

---
+++ batch-process-Hire.sh
@@ -0,0 +1,11 @@
+#! /bin/sh
+for f in "$1"/*; do
+  if [ -f "$f" ]; then
+    SIZE="$(du -sh "${f}" | cut -f1)"
+    echo "Processing $f file..."
+    echo "File size: $SIZE"
+    NUMWORDS="$(wc -w "${f}" | cut -d ' ' -f1 )"
+    echo "Word count: $NUMWORDS"
+  fi
+done
+
